5131896f00abff7adc18863fcc1097e7f05d49ea
Shishobakshosho <mmmfan@abv.bg>
2015-12-15 15:04:44 +0200

    Update Synchro.java

--- LastVersionTicTacToe/src/common/Synchro.java
+++ LastVersionTicTacToe/src/common/Synchro.java
@@ -7,7 +7,7 @@ import java.net.Socket;
 
 public class Synchro {
 
-	final public int[][] winPositions = new int[][] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 },
+	static final public int[][] winPositions = new int[][] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 }, { 1, 4, 7 },
 			{ 2, 5, 8 }, { 0, 4, 8 }, { 2, 4, 6 } };
 
 	private boolean playerTwoTurn = false;
